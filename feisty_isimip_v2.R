860de82af79fc15e88a5a1e0c126eab0af8b708e
Alexandra Cabanelas <acabanelas@whoi.edu>
2025-04-24 19:39:26 -0400

    loop to clip all 66 lmes

--- feisty_isimip_v2.R
+++ feisty_isimip_v2.R
@@ -10,14 +10,16 @@
 ## ------------------------------------------ ##
 #            Packages -----
 ## ------------------------------------------ ##
-library(ncdf4) #to open netcdf
-library(tidyverse)
-library(lubridate)
-library(terra) # for bilinear interpolation
-library(sf)
-library(stars)
-library(gganimate)
-library(gifski)
+library(ncdf4) #to open netcdf; v1.21
+library(tidyverse) #v2.0.0
+library(lubridate) #v1.9.3
+library(terra) # for bilinear interpolation; #v1.7.29
+library(sf) #v1.0.15
+library(stars) #v0.6.2
+library(exactextractr) #v0.10.0
+
+library(gganimate) #v1.0.8
+library(gifski) #v1.32.0.2
 
 ## ------------------------------------------ ##
 #            Data -----
@@ -98,6 +100,8 @@ length(lonzoo)
 range(latzoo) # 1 degree spacing ;-89.5  89.5
 length(latzoo)
 
+zoo_slice <- ncvar_get(zoomss, "tcb", start = c(1, 1, 1), count = c(-1, -1, 1))
+image.plot(lonzoo, latzoo, zoo_slice)
 ## ZOOMSS IS ONE DEGREEEEEEEE
 
 feisty_stars <- st_as_stars(feisty_stack) 
@@ -121,25 +125,46 @@ names(lme)
 head(lme)
 
 # we are using LME #s
-## 10 = HOTS
+## 10 = Hawaii
 ## 14 = Patagonian Shelf
 ## 21 = Norwegian Shelf
 ## 28 = Guinea Current 
 
-lme_HOTS <- lme %>% 
-  filter(LME_NUMBER %in% c(10))
 lme_patagonia <- lme %>% 
   filter(LME_NUMBER %in% c(14))
-lme_nor <- lme %>% 
-  filter(LME_NUMBER %in% c(21))
-lme_guinea <- lme %>% 
-  filter(LME_NUMBER %in% c(28))
 
-
-
+patagonia_mean <- lapply(1:nlyr(feisty_stack), function(i) {
+  exact_extract(feisty_stack[[i]], lme_patagonia, 
+                fun = 'weighted_mean', weights = "area")
+})
 annual_weighted_means <- lapply(1:dim(src_resampled)[3], function(i) {
   exact_extract(src_resampled[,,,i], shape, 'weighted_mean')
 })
 
+####################
+# 3a) All 66 LMEs - loop -----
+####################
+# empty list
+lme_results <- vector("list", length = nrow(lme))
 
-#clip
+for (i in seq_len(nrow(lme))) {
+  message("Processing LME: ", lme$LME_NAME[i])
+  
+  lme_poly <- lme[i, ]  # single LME as sf
+  
+  means <- lapply(1:nlyr(feisty_stack), function(j) {
+    exact_extract(feisty_stack[[j]], lme_poly, 
+                  fun = "weighted_mean", weights = "area")
+  })
+  
+  lme_results[[i]] <- tibble(
+    LME_NUMBER = lme$LME_NUMBER[i],
+    LME_NAME = lme$LME_NAME[i],
+    year = 1950:2014,
+    tcb = unlist(means)
+  )
+}
+
+lme_all_df <- bind_rows(lme_results)
+
+
